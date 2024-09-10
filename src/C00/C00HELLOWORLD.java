90414ef053421e750131fa8029b9ea6e16d5b6c4
junyoung6 <rlrpqkd12@naver.com>
2024-09-10 17:48:59 +0900

    init

---
+++ src/C00/C00HELLOWORLD.java
@@ -0,0 +1,20 @@
+package C00;  // 패키지명
+
+import java.util.Iterator;
+
+public class C00HELLOWORLD   // 클래스 영역 = 객체지향 문법 적용단위 
+{
+	// 메서드(함수) : 작업수행을 하는데 팔요한 여러 코드를 묶어서 실행하는 단위
+	// main 메서드 : 최초 실행 함수
+	// library 메서드 : 미리 만들어저 제공되어지는 메서드
+	// 사용자 정의 메서드 : 개발자에 의해 만들어지는 메서드
+	// 함수(data) : 수를 담아 특정처리를 하는 논리적 공간
+	public static void main(String[] args)  // 메서드 영역 - 절차지향 문법 적용 단위
+	{
+		System.out.println("HELLO WORLD"); // 라이브 러리 호출
+	   
+		
+		
+	}
+
+}
