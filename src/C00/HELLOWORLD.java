4001ab636e3ad33318d3a36f998f55ff7bcab702
junyoung6 <rlrpqkd12@naver.com>
2024-09-10 16:46:15 +0900

    init

---
+++ src/C00/HELLOWORLD.java
@@ -0,0 +1,15 @@
+package C00;
+
+import java.util.Iterator;
+
+public class HELLOWORLD {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HELLO WORLD");
+	   
+		
+		
+	}
+
+}
